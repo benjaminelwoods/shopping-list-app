6b2fbcd641cc8aef3b1d914eb9e376fa7e6944a5
benjaminelwoods <ben.elwoods@gmail.com>
2020-09-17 19:57:23 +1200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="">
+      <div class="inner">
+        <li v-for="item in items" :key="item.id">{{item.content}}</li>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import firebase from 'firebase'
+console.log(firebase)
+import db from '../firebase.js'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  // components: {
+  //   // HelloWorld
+  // }
+  data () {
+    return {
+
+    }
+  },
+  created () {
+    db.collection('lists').doc('test').get().then(function (doc) {
+      console.log(doc.data())
+    })
   }
 }
 </script>
