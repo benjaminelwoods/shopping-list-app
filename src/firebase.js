7766279dbcecdb1f006320afcfb279360e04f414
benjaminelwoods <ben.elwoods@gmail.com>
2020-09-18 13:41:32 +1200

    latest

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,6 @@ var firebase = require('firebase')
 
 var firebaseObj = {
   firebaseConfig: {
-    apiKey: 'AIzaSyAQPC75PeBM1KxIoKPJln9fs7SXonc-Jxw',
-    authDomain: 'shopping-list-app-74fc5.firebaseapp.com',
-    databaseURL: 'https://shopping-list-app-74fc5.firebaseio.com',
-    projectId: 'shopping-list-app-74fc5',
-    storageBucket: 'shopping-list-app-74fc5.appspot.com',
-    messagingSenderId: '842871118984',
-    appId: '1:842871118984:web:f33e7cef63abc99a88933d'
   },
   getDb: function () {
     const firebaseApp = firebase.initializeApp(firebaseObj.firebaseConfig)
